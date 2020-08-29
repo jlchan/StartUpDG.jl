0fb532d21fe44229ea76456034743e4efa09d311
Jesse Chan <jesse.chan@rice.edu>
2020-08-28 23:05:46 -0500

    Files generated by PkgTemplates

---
+++ src/StartUpDG.jl
@@ -0,0 +1,5 @@
+module StartUpDG
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StartUpDG
+using Test
+
+@testset "StartUpDG.jl" begin
+    # Write your own tests here.
+end
