70c48496ecac831b4d7ee821c5d355320c9db445
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 21:17:31 -0500

    adding map_face_nodes

--- src/ref_elem_utils.jl
+++ src/ref_elem_utils.jl
@@ -44,13 +44,35 @@ face_vertices(elem) = find_face_nodes(elem,nodes(elem,1)...)
 ##### face data for diff elements
 #####
 
+function map_face_nodes(elem::Tri, face_nodes)
+    r1D = face_nodes
+    e = ones(size(r1D)) # vector of all ones
+    rf = [r1D; -r1D; -e];
+    sf = [-e; r1D; -r1D];
+    return rf,sf
+end
+function map_face_nodes(elem::Quad, face_nodes)
+    r1D = face_nodes
+    e = ones(size(r1D))
+    rf = [r1D; e; -r1D; -e]
+    sf = [-e; r1D; e; -r1D]
+    return rf,sf
+end
+function map_face_nodes(elem::Hex, face_nodes...)
+    rquad,squad = face_nodes
+    e = ones(size(rquad))
+    rf = [-e; e; rquad; rquad; rquad; rquad]
+    sf = [rquad; rquad; -e; e; squad; squad]
+    tf = [squad; squad; squad; squad; -e; e]
+    return rf,sf,tf
+end
+
 function init_face_data(elem::Tri, N; quad_nodes_face=gauss_quad(0,0,N))
     #Nodes on faces, and face node coordinate
     r1D, w1D = quad_nodes_face
     e = ones(size(r1D)) # vector of all ones
     z = zeros(size(r1D)) # vector of all zeros
-    rf = [r1D; -r1D; -e];
-    sf = [-e; r1D; -r1D];
+    rf,sf = map_face_nodes(elem,r1D)
     wf = vec(repeat(w1D,3,1));
     nrJ = [z; e; -e]
     nsJ = [-e; e; z]
@@ -61,8 +83,7 @@ function init_face_data(elem::Quad,N; quad_nodes_face=gauss_quad(0,0,N))
     r1D,w1D = quad_nodes_face
     e = ones(size(r1D))
     z = zeros(size(r1D))
-    rf = [r1D; e; -r1D; -e]
-    sf = [-e; r1D; e; -r1D]
+    rf,sf = map_face_nodes(elem,r1D)
     wf = vec(repeat(w1D,4,1)); # 4 faces
     nrJ = [z; e; z; -e]
     nsJ = [-e; z; e; z]
@@ -73,9 +94,7 @@ function init_face_data(elem::Hex, N)
     rquad,squad,wquad = quad_nodes(Quad(),N)
     e = ones(size(rquad))
     zz = zeros(size(rquad))
-    rf = [-e; e; rquad; rquad; rquad; rquad]
-    sf = [rquad; rquad; -e; e; squad; squad]
-    tf = [squad; squad; squad; squad; -e; e]
+    rf,sf,tf = map_face_nodes(elem,rquad,squad)
     Nfaces = 6
     wf = vec(repeat(wquad,Nfaces,1));
     nrJ = [-e; e; zz;zz; zz;zz]
