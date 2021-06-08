ab9bb88434969277bbbbbb72ab0e5a2ce4c93c97
Jesse Chan <jesse.chan@rice.edu>
2021-06-07 21:19:52 -0500

    adding trace constants

--- src/ref_elem_utils.jl
+++ src/ref_elem_utils.jl
@@ -60,3 +60,26 @@ function map_face_nodes(elem::Hex, face_nodes...)
     return rf,sf,tf
 end
 
+
+
+"""
+    function inverse_trace_constant(rd::RefElemData)
+
+Returns inverse trace constant as reported in ["GPU-accelerated dG methods on hybrid meshes"](https://doi.org/10.1016/j.jcp.2016.04.003)
+by Chan, Wang, Modave, Remacle, Warburton 2016. 
+"""
+inverse_trace_constant(rd::RefElemData{1}) = (rd.N+1)*(rd.N+2)/2
+inverse_trace_constant(rd::RefElemData{1,Line,SBP}) = rd.N*(rd.N+1)/2 # assumes SBP <=> DGSEM
+inverse_trace_constant(rd::RefElemData{2,Quad}) = (rd.N+1)*(rd.N+2)
+inverse_trace_constant(rd::RefElemData{2,Quad,SBP}) = rd.N*(rd.N+1) # assumes SBP <=> DGSEM
+inverse_trace_constant(rd::RefElemData{3,Hex}) = 3*(rd.N+1)*(rd.N+2)/2
+inverse_trace_constant(rd::RefElemData{3,Hex,SBP}) = 3*rd.N*(rd.N+1)/2 # assumes SBP <=> DGSEM
+
+# precomputed
+_inverse_trace_constants(rd::RefElemData{2,Tri,Polynomial}) = (5.999999999999999, 10.898979485566365, 16.292060161853993, 23.999999999999808, 31.884512140579055, 42.42373503225737, 52.88579066878113, 66.25284319164409, 79.3535377715693, 95.53911875636945)
+inverse_trace_constant(rd::RefElemData{2,Tri,Polynomial}) = _inverse_trace_constants(rd)[rd.N]
+
+# generic fallback
+function inverse_trace_constant(rd::RefElemData)
+    return maximum(eigvals(Matrix(rd.Vf'*diagm(rd.wf)*rd.Vf),Matrix(rd.M)))
+end
