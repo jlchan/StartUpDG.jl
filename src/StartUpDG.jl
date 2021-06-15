6004c39ffd3e488e1dd0b1de1291a37263ec52cc
Jesse Chan <jesse.chan@rice.edu>
2021-06-15 15:24:19 -0500

    updates for Triangulate support

--- src/StartUpDG.jl
+++ src/StartUpDG.jl
@@ -12,28 +12,45 @@ using Reexport
 using NodesAndModes: meshgrid
 using SparseArrays: sparse, droptol!
 using MAT # read in .mat files
+using RecipesBase, Colors 
 
 # reference element utility functions
-export RefElemData, PolynomialRefElemData, Polynomial, SBP
-export hybridized_SBP_operators, inverse_trace_constant
 include("RefElemData.jl")
 include("RefElemData_polynomial.jl")
 include("RefElemData_SBP.jl")
 include("ref_elem_utils.jl")
+export RefElemData, PolynomialRefElemData, Polynomial, SBP
+export hybridized_SBP_operators, inverse_trace_constant
 
-export MeshData 
 include("MeshData.jl")
+export MeshData 
 
-export geometric_factors, estimate_h
 include("geometric_functions.jl")
+export geometric_factors, estimate_h
 
 # spatial connectivity routines
-export make_periodic
 include("connectivity_functions.jl")
+export make_periodic
 
 # uniform meshes + face vertex orderings are included
+include("mesh/simple_meshes.jl")
 export readGmsh2D, uniform_mesh
-include("simple_meshes.jl")
+
+include("mesh/mesh_visualization.jl")
+export MeshPlotter, MeshPlotter
+
+using Requires
+using Printf
+function __init__()                 
+    @require Triangulate="f7e6ffb2-c36d-4f8f-a77e-16e897189344" begin
+        using .Triangulate: TriangulateIO, triangulate
+        include("mesh/triangulate_utils.jl")      
+        export refine, triangulateIO_to_VXYEToV, get_node_boundary_tags
+        export BoundaryTagPlotter
+        include("mesh/triangulate_example_meshes.jl")
+        export square_domain, rectangular_domain, square_hole_domain, scramjet
+    end
+end
 
 # simple explicit time-stepping included for conveniencea
 export ck45, dp56, PIparams, compute_adaptive_dt, bcopy! # LSERK 45 + Dormand-Prince 56
