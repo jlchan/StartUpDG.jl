8cc59cfdb1528a30ab44b0749934783250aa778e
Jesse Chan <jesse.chan@rice.edu>
2021-06-15 22:46:59 -0500

    add smaller show method

--- src/RefElemData.jl
+++ src/RefElemData.jl
@@ -48,10 +48,22 @@ struct RefElemData{Dim, ElemShape <: AbstractElemShape, ApproximationType, Nface
     LIFT::L             # lift matrix
 end
 
-function Base.show(io::IO, rd::RefElemData)
+function Base.show(io::IO, ::MIME"text/plain", rd::RefElemData)
     @nospecialize rd
     print(io,"RefElemData for a degree $(rd.N) $(rd.approximationType) approximation on $(rd.elementType) element.")
 end
+
+
+function Base.show(io::IO, rd::RefElemData)
+  @nospecialize basis # reduce precompilation time
+  print(io,"RefElemData{polydeg=$(rd.N),$(rd.approximationType),$(rd.elementType)}.")
+end
+function Base.show(io::IO, ::MIME"text/plain", basis::LobattoLegendreBasis)
+  @nospecialize basis # reduce precompilation time
+
+  print(io, "LobattoLegendreBasis{", real(basis), "} with polynomials of degree ", polydeg(basis))
+end
+
 
 _propertynames(::Type{RefElemData},private::Bool=false) = (:Nfaces, :Np, :Nq, :Nfq)
 function Base.propertynames(x::RefElemData{1},private::Bool=false) 
