775e4805fc31b7bf3d0376e0f32157607566994c
Jesse Chan <jesse.chan@rice.edu>
2021-08-21 18:17:51 -0500

    adding local mesh size estimate

--- src/geometric_functions.jl
+++ src/geometric_functions.jl
@@ -54,23 +54,30 @@ function geometric_factors(x, y, z, Dr, Ds, Dt, Filters=(I,I,I))
 end
 
 """
-    estimate_h(rd::RefElemData,md::MeshData)
+    estimate_h(rd::RefElemData, md::MeshData)
+    estimate_h(e, rd::RefElemData, md::MeshData) # e = element index
 
 Estimates the mesh size via min size_of_domain * |J|/|sJ|, since |J| = O(hᵈ) and |sJ| = O(hᵈ⁻¹). 
 """
 function estimate_h(rd::RefElemData{DIM}, md::MeshData{DIM}) where {DIM}
     hmin = Inf
     for e in 1:md.num_elements
-        sJ_e = reshape(view(md.sJ, :, e), rd.Nfq ÷ rd.Nfaces, rd.Nfaces)
-        sJ_face = zero(eltype(md.sJ))
-        for f in 1:rd.Nfaces
-            sJ_face = max(sJ_face, minimum(view(sJ_e, :, f)) / face_scaling(rd, f))
-        end
-        h_e = minimum(view(md.J, :, e)) / sJ_face
+        h_e = estimate_h(e, rd, md)
         hmin = min(hmin, h_e)
     end
     return hmin * compute_domain_size(rd, md)^(1/DIM)
 end
+
+function estimate_h(e, rd::RefElemData, md::MeshData)
+    sJ_e = reshape(view(md.sJ, :, e), rd.Nfq ÷ rd.Nfaces, rd.Nfaces)
+    sJ_face = zero(eltype(md.sJ))
+    for f in 1:rd.Nfaces
+        sJ_face = max(sJ_face, minimum(view(sJ_e, :, f)) / face_scaling(rd, f))
+    end
+    h_e = minimum(view(md.J, :, e)) / sJ_face
+    return h_e
+end
+
 face_scaling(rd, f) = 1.0
 face_scaling(rd::RefElemData{2, Tri}, f) = f == 3 ? sqrt(2) : 1.0 # sJ incorporates length of long triangle edge
 face_scaling(rd::RefElemData{3, Tet}, f) = f == 2 ? sqrt(3) : 1.0 # sJ incorporates area of larger triangle face
