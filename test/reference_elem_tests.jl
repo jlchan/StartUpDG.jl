0d2e96b8fb332dda3c63695932aeb21c3f052b37
Jesse Chan <jesse.chan@rice.edu>
2022-04-18 19:51:48 -0500

    fix formatting

--- test/reference_elem_tests.jl
+++ test/reference_elem_tests.jl
@@ -7,14 +7,14 @@
         rd = RefElemData(Line(),N)
         @test rd.r == rd.rst[1]
         @test rd.Np == length(rd.r)
-        @test abs(sum(rd.rq.*rd.wq)) < tol
+        @test abs(sum(rd.rq .* rd.wq)) < tol 
         @test rd.nrJ ≈ [-1,1]
-        @test rd.Pq*rd.Vq ≈ I
+        @test rd.Pq * rd.Vq ≈ I
         @test rd.r[rd.Fmask[:]] ≈ rd.rf
-        @test invoke(inverse_trace_constant,Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)
+        @test invoke(inverse_trace_constant, Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)
         @test propertynames(rd)[1] == :elementType
-        @test rd.elementType==rd.element_type
-        @test rd.approximationType==rd.approximation_type
+        @test rd.elementType == rd.element_type
+        @test rd.approximationType == rd.approximation_type
     end
 
     @testset "Triangle" begin
@@ -25,11 +25,11 @@
         @test abs(sum(rd.wq)) ≈ 2
         @test abs(sum(rd.wf)) ≈ 6
         @test abs(sum(rd.wf .* rd.nrJ)) + abs(sum(rd.wf .* rd.nsJ)) < tol
-        @test rd.Pq*rd.Vq ≈ I
-        Vfp = vandermonde(Line(),N,quad_nodes(Line(),N)[1])/vandermonde(Line(),N,nodes(Line(),N))
-        rstf = (x->Vfp*x[reshape(rd.Fmask,rd.Nfq÷rd.Nfaces,rd.Nfaces)]).(rd.rst)
+        @test rd.Pq * rd.Vq ≈ I
+        Vfp = vandermonde(Line(), N, quad_nodes(Line(), N)[1]) / vandermonde(Line(), N, nodes(Line(), N))
+        rstf = (x->Vfp * x[reshape(rd.Fmask, rd.Nfq ÷ rd.Nfaces, rd.Nfaces)]).(rd.rst)
         @test all(vec.(rstf) .≈ rd.rstf)
-        @test invoke(inverse_trace_constant,Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)
+        @test invoke(inverse_trace_constant,Tuple{RefElemData}, rd) ≈ inverse_trace_constant(rd)
         @test propertynames(rd)[1] == :elementType
     end
 
@@ -41,11 +41,11 @@
         @test abs(sum(rd.wq)) ≈ 4
         @test abs(sum(rd.wf)) ≈ 8
         @test abs(sum(rd.wf .* rd.nrJ)) + abs(sum(rd.wf .* rd.nsJ)) < tol
-        @test rd.Pq*rd.Vq ≈ I
-        Vfp = vandermonde(Line(),N,quad_nodes(Line(),N)[1])/vandermonde(Line(),N,nodes(Line(),N))
-        rstf = (x->Vfp*x[reshape(rd.Fmask,rd.Nfq÷rd.Nfaces,rd.Nfaces)]).(rd.rst)
+        @test rd.Pq * rd.Vq ≈ I
+        Vfp = vandermonde(Line(), N, quad_nodes(Line(), N)[1]) / vandermonde(Line(), N, nodes(Line(), N))
+        rstf = (x->Vfp * x[reshape(rd.Fmask,rd.Nfq÷rd.Nfaces,rd.Nfaces)]).(rd.rst)
         @test all(vec.(rstf) .≈ rd.rstf)
-        @test invoke(inverse_trace_constant,Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)    
+        @test invoke(inverse_trace_constant, Tuple{RefElemData}, rd) ≈ inverse_trace_constant(rd)    
     end
 
     @testset "Hex" begin
@@ -64,8 +64,8 @@
         @test abs(sum(rd.wf .* rd.nrJ)) < tol
         @test abs(sum(rd.wf .* rd.nsJ)) < tol
         @test abs(sum(rd.wf .* rd.ntJ)) < tol
-        @test rd.Pq*rd.Vq ≈ I
-        @test invoke(inverse_trace_constant,Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)
+        @test rd.Pq * rd.Vq ≈ I
+        @test invoke(inverse_trace_constant, Tuple{RefElemData}, rd) ≈ inverse_trace_constant(rd)
         # TODO: test interpolation of Fmask matches rd.rstf.
     end
 
@@ -85,7 +85,7 @@
         @test abs(sum(rd.wf .* rd.nrJ)) < tol
         @test abs(sum(rd.wf .* rd.nsJ)) < tol
         @test abs(sum(rd.wf .* rd.ntJ)) < tol
-        @test rd.Pq*rd.Vq ≈ I
-        @test invoke(inverse_trace_constant,Tuple{RefElemData},rd) ≈ inverse_trace_constant(rd)
+        @test rd.Pq * rd.Vq ≈ I
+        @test invoke(inverse_trace_constant, Tuple{RefElemData}, rd) ≈ inverse_trace_constant(rd)
     end
 end
