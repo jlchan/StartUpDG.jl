c4b08f0df2b5bb52ffc505ce224993b78e7da6b5
Jesse Chan <jesse.chan@rice.edu>
2022-04-27 16:15:39 -0500

    fix test again (orientation/negative jacobian issue)

--- test/hybrid_mesh_tests.jl
+++ test/hybrid_mesh_tests.jl
@@ -11,13 +11,13 @@
     #     -1     0     1
     VX = [-1; 0; 1; -1; 0; 1; -1; 0; 1]
     VY = [-1; -1; -1; 0; 0; 0; 1; 1; 1]
-    EToV = [[1 2 4 5], [2 3 5 6], [9 8 5], [4 5 7 8], [5 6 9]]
+    EToV = [[1 2 4 5], [2 3 5 6], [5 8 9], [4 5 7 8], [9 6 5]]
 
     rds = Dict((elem => RefElemData(N=2, elem) for elem in (Tri(), Quad())))
 
     fvs = Dict(Pair.(keys(rds), getproperty.(values(rds), :fv)))
     FToF = StartUpDG.connect_mesh(EToV, fvs)
 
-    @test FToF == vec([1  5  3  14  2  6  7  18  16  13  11  12  10  4  15  9  17  8])
+    @test FToF == vec([1  5  3  14  2  6  7  17  16  10  13  12  11  4  15  9  8  18])
 
 end
