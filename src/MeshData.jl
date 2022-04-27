03e1c1cc310f0b3ef0e685d46161022923ff059b
Jesse Chan <jesse.chan@rice.edu>
2022-04-27 00:41:26 -0500

    generalizing MeshData field types

--- src/MeshData.jl
+++ src/MeshData.jl
@@ -16,8 +16,7 @@ md = MeshElemData(VXY, EToV, rd)
 Base.@kwdef struct MeshData{Dim, VolumeType, FaceType,                             
                             VertexType, EToVType, FToFType, 
                             VolumeWeightType, VolumeGeofacsType, VolumeJType,
-                            ConnectivityType, BoundaryMapType, 
-                            DimTimesDim}
+                            ConnectivityType, BoundaryMapType}
 
     # num_elements::Ti            # number of elements
     VXYZ::NTuple{Dim, VertexType}  # vertex coordinates
@@ -35,7 +34,7 @@ Base.@kwdef struct MeshData{Dim, VolumeType, FaceType,
     mapB::BoundaryMapType
 
     # volume geofacs Gij = dx_i/dxhat_j
-    rstxyzJ::SMatrix{Dim, Dim, VolumeGeofacsType, DimTimesDim}
+    rstxyzJ::VolumeGeofacsType
     J::VolumeJType
 
     # surface geofacs
