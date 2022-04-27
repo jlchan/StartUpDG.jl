17487d9185c6511521aac9c4b491b964264a0371
Jesse Chan <jesse.chan@rice.edu>
2022-04-27 09:44:16 -0500

    larger test mesh

--- test/hybrid_mesh_tests.jl
+++ test/hybrid_mesh_tests.jl
@@ -1,21 +1,23 @@
 @testset "Hybrid mesh utilities" begin
     # Simple hybrid mesh for testing
-    #   1  4_____5_____6
+    #   1  7_____8_____9
     #      |     |   / |
     #      |     | /   |
-    #   0  1 --- 2 --- 3 
+    #   0  4 --- 5 --- 6 
+    #      |     |     |
+    #      |     |     |
+    #   -1 1 --- 2 --- 3
+    #     
     #     -1     0     1
-    VX = [-1; 0; 1; -1; 0; 1]
-    VY = [0; 0; 0; 1; 1; 1]
-    EToV = [[1 2 4 5], [2 3 6], [6 5 2]]
+    VX = [-1; 0; 1; -1; 0; 1; -1; 0; 1]
+    VY = [-1; -1; -1; 0; 0; 0; 1; 1; 1]
+    EToV = [[1 2 4 5], [2 3 5 6], [4 5 7 8], [2 3 6], [6 5 2]]
 
-    # rd = (RefElemData(N=2, Tri()), RefElemData(N=2, Quad()))
-    # fvs = Dict(Pair.(getproperty.(rd, :elementType), getproperty.(rd, :fv)))
-    rd = Dict((elem => RefElemData(N=2, elem) for elem in (Tri(), Quad())))
-    fvs = Dict(Pair.(keys(rd), getproperty.(values(rd), :fv)))
+    rds = Dict((elem => RefElemData(N=2, elem) for elem in (Tri(), Quad())))
+    fvs = Dict(Pair.(keys(rds), getproperty.(values(rds), :fv)))
 
     FToF = StartUpDG.connect_mesh(EToV, fvs)
 
-    @test FToF == [1, 9, 3, 4, 8, 6, 7, 5, 2, 10]
+    @test FToF == vec([1  5  3  11  17  14  15  18  9  10  4  12  16  6  7  13  2  8])
 
 end
