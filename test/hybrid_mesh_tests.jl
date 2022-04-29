a40dce54768e379a31dff0dad57afe2846abda07
Jesse Chan <jesse.chan@rice.edu>
2022-04-29 10:44:50 -0500

    update tests

--- test/hybrid_mesh_tests.jl
+++ test/hybrid_mesh_tests.jl
@@ -1,4 +1,19 @@
 @testset "Hybrid mesh utilities" begin
+
+    rds = Dict((elem => RefElemData(N=2, elem) for elem in (Tri(), Quad())))
+
+    #   1  3_______4______5
+    #      |   4   |  6  / 
+    #      |1     2|7  5
+    #      |   3   |  /  
+    #  -1  1 ----- 2        1
+
+    VX = [-1; 0; -1; 0; 1]
+    VY = [-1; -1; 1; 1; 1]
+    EToV = [[1 2 3 4], [2 4 5]]
+    md = MeshData(VX, VY, EToV, rds)
+    @test md.FToF == vec([1  7  3  4  5  6  2])
+
     # Simple hybrid mesh for testing
     #   1  7______8______9
     #      |      | 3  / |
@@ -13,11 +28,8 @@
     VY = [-1; -1; -1; 0; 0; 0; 1; 1; 1]
     EToV = [[1 2 4 5], [2 3 5 6], [5 8 9], [4 5 7 8], [9 6 5]]
 
-    rds = Dict((elem => RefElemData(N=2, elem) for elem in (Tri(), Quad())))
-
-    fvs = Dict(Pair.(keys(rds), getproperty.(values(rds), :fv)))
-    FToF = StartUpDG.connect_mesh(EToV, fvs)
-
-    @test FToF == vec([1  5  3  14  2  6  7  17  16  10  13  12  11  4  15  9  8  18])
+    md = MeshData(VX, VY, EToV, rds)
+    @test md.FToF == vec([1  5  3  14  2  6  7  17  16  10  13  12  11  4  15  9  8  18])
+    @test max.(abs.(md.xf[md.mapB]), abs.(md.yf[md.mapB])) .== 1
 
 end
