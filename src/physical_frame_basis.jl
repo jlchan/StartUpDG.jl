505b977e1e0014511f3a40c8f0e1b66e06a32ca1
Jesse Chan <jesse.chan@rice.edu>
2022-09-20 16:37:27 -0500

    adding antidiff operators and phys-frame basis

--- src/physical_frame_basis.jl
+++ src/physical_frame_basis.jl
@@ -1,3 +1,6 @@
+# dimension of a cut cell polynomial space
+@inline Np_cut(N) = (N + 1) * (N + 2) ÷ 2 
+
 @inline mean(x) = sum(x) / length(x)
 
 """
@@ -17,7 +20,7 @@ end
 
 Constructor for a PhysicalFrame object given arrays of points `x`, `y` on a cut element. 
 """
-# default shiftinging and scaling
+# default shifting and scaling
 PhysicalFrame() = PhysicalFrame(SVector(0.0, 0.0), SVector(1.0, 1.0))
 
 function PhysicalFrame(x, y)
@@ -26,7 +29,7 @@ function PhysicalFrame(x, y)
     return PhysicalFrame(shifting, scaling)
 end
 
-function NodesAndModes.vandermonde(elem::PhysicalFrame, N, x, y)
+function NodesAndModes.basis(elem::PhysicalFrame, N, x, y)
     Np = (N + 1) * (N + 2) ÷ 2
 
     @unpack shifting, scaling = elem
@@ -34,18 +37,64 @@ function NodesAndModes.vandermonde(elem::PhysicalFrame, N, x, y)
     s = @. (y - shifting[2]) * scaling[2]
 
     sk = 1
-    # V, Vr, Vs = ntuple(x->zeros(length(r), Np), 3)
-    V = zeros(length(r), Np)
+    V, Vr, Vs = ntuple(x->zeros(length(r), Np), 3)
     for j = 0:N
         P_j = jacobiP(s, 0, 0, j)
         for i = 0:N-j
             P_i = jacobiP(r, 0, 0, i)
             V[:, sk]  = P_i .* P_j
-            # Vr[:, sk] = grad_jacobiP(r, 0, 0, i) .* P_j * scaling[1]
-            # Vs[:, sk] = P_i .* grad_jacobiP(s, 0, 0, j) * scaling[2]
+            Vr[:, sk] = grad_jacobiP(r, 0, 0, i) .* P_j * scaling[1]
+            Vs[:, sk] = P_i .* grad_jacobiP(s, 0, 0, j) * scaling[2]
             sk += 1
         end
+    end    
+    return V, Vr, Vs
+end
+
+# integrated Legendre polynomials. basis = degree N 1D basis
+# see also https://onlinelibrary.wiley.com/doi/abs/10.1002/nme.1620280813
+function antidiff_basis_1D(basis_1D, N, r)
+    rq, wq = gauss_quad(0, 0, N+1) 
+    Vout = zeros(eltype(r), length(r), N+1)
+    for i in eachindex(r)
+        h = r[i] + 1
+        Vout[i, :] .= h/2 .* vec(wq' * basis_1D(N, map_to_interval.(rq, -1, r[i])))
     end
-    return V
-    # return V, Vr, Vs
+    return Vout
+end
+
+function antidiff_basis_2D(N, r, s)
+    Ix, Iy = ntuple(_ -> zeros(eltype(r), length(r), Np_cut(N)), 2)
+    Vr, _ = basis(Line(), N, r)
+    Vs, _ = basis(Line(), N, s)
+    basis_1D = (N, r) -> basis(Line(), N, r)[1]
+    Ir = antidiff_basis_1D(basis_1D, N, r)
+    Is = antidiff_basis_1D(basis_1D, N, s)
+
+    sk = 1
+    for j in 0:N, i in 0:N-j
+        Ix[:, sk] .= Ir[:, i+1] .* Vs[:, j+1]
+        Iy[:, sk] .= Vr[:, i+1] .* Is[:, j+1]
+        sk += 1
+    end
+    return Ix, Iy
+end
+
+# computes antidifferentiation operators for total degree N bases 
+# on the reference quadrilateral element. These operators map from 
+# the space of total degree N to total degree N+1 polynomials. 
+function antidiff_operators(N)
+    # antidiff basis: P^N -> nodes
+    # inv(VNp1): nodes -> P^{N+1}
+    #   => inv(VNp1) * antidiff basis: P^N -> P^{N+1}
+    r, s = nodes(Quad(), N+1)
+    VNp1, VNp1r, VNp1s = basis(PhysicalFrame(), N+1, r, s)
+    Ir, Is = map(A -> VNp1 \ A, antidiff_basis_2D(N, r, s))
+
+    # check that the antidiff operator is the pseudo-inverse of the P^{N+1} -> P^N differentiation matrix
+    V, _ = basis(PhysicalFrame(), N, r, s)
+    Dr, Ds = V \ VNp1r, V \ VNp1s 
+    @assert norm(Dr * Ir - I) + norm(Ds * Is - I) < 100 * eps() * size(Ir, 1)
+
+    return Ir, Is
 end
