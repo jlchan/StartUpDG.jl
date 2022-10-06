53d5eb47278ca0b248e4325fb93094417472a9bf
Jesse Chan <jesse.chan@rice.edu>
2022-10-06 05:31:33 -0500

    switching to built-in unpacking in a few places

--- src/nonconforming.jl
+++ src/nonconforming.jl
@@ -9,7 +9,7 @@ The intended usage is as follows:
 rd = RefElemData(Quad(), N=7)
 md = MeshData(NonConformingQuadMeshExample(), rd)
 
-@unpack x, y = md
+(; x, y) = md
 u = @. sin(pi * x) * sin(pi * y)
 
 # interpolate to faces
@@ -17,7 +17,7 @@ num_total_faces = num_faces(rd.element_type) * md.num_elements
 u_face = reshape(rd.Vf * u, :, num_total_faces)
 
 # interpolate faces to mortars (`uf` denotes mortar faces for `NonConformingMesh` types)
-@unpack conforming_faces, non_conforming_faces, mortar_interpolation_matrix = md.mesh_type
+(; conforming_faces, non_conforming_faces, mortar_interpolation_matrix) = md.mesh_type
 u_mortar = similar(md.xf)
 view(u_mortar, :, 1:length(conforming_faces)) .= view(u_face, :, conforming_faces)
 
--- test/noncon_mesh_tests.jl
+++ test/noncon_mesh_tests.jl
@@ -3,7 +3,7 @@
     rd = RefElemData(Quad(), N=3)
     md = MeshData(NonConformingQuadMeshExample(), rd)
 
-    @unpack x, y = md
+    (; x, y) = md
     u = @. sin(pi * x) * sin(pi * y)
 
     # interpolate to faces
@@ -11,10 +11,10 @@
     u_face = reshape(rd.Vf * u, :, num_total_faces)
 
     # interpolate faces to mortars (`uf` denotes mortar faces for `NonConformingMesh` types)
-    @unpack conforming_faces, non_conforming_faces, mortar_interpolation_matrix = md.mesh_type
+    (; conforming_faces, non_conforming_faces, mortar_interpolation_matrix) = md.mesh_type
 
     u_mortar = similar(md.xf)
-    view(u_mortar, :, 1:length(conforming_faces)) .= view(u_face, :, conforming_faces)
+    view(u_mortar, :, 1:length(conforming_faces)) .= view(u_face, :, conforming_faces)    
     # interpolate to non-conforming faces
     for (i, f) in enumerate(non_conforming_faces)
         mortar_face_ids = (1:num_mortars_per_face(rd)) .+ (i-1) * num_mortars_per_face(rd) .+ length(conforming_faces)
