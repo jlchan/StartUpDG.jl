6bde84faa11ce259669ccb7f2d79687562a47ec7
Jesse Chan <jesse.chan@rice.edu>
2022-10-19 21:15:50 -0500

    add curved hybrid mesh tests
    d

--- src/hybrid_meshes.jl
+++ src/hybrid_meshes.jl
@@ -9,6 +9,21 @@ struct HybridMesh{T}
     end
 end
 
+function HybridMeshExample()
+    # Simple hybrid mesh for testing
+    #   1  7______8______9
+    #      |      | 3  / |
+    #      |   4  |  / 5 |
+    #   0  4 ---- 5 ---- 6 
+    #      |      |      |
+    #      |   1  |   2  |
+    #   -1 1 ---- 2 ---- 3
+    #     -1      0      1
+    VX = [-1; 0; 1; -1; 0; 1; -1; 0; 1]
+    VY = [-1; -1; -1; 0; 0; 0; 1; 1; 1]
+    EToV = [[1 2 4 5], [2 3 5 6], [5 8 9], [4 5 7 8], [9 6 5]]
+    return (VX, VY), EToV
+end
 
 # Given a tuple of element types, find which element type has 
 # `num_vertices_of_target` vertices. 
