9ee724dbebfa349b2dfad125ca13009acd54fb82
Jesse Chan <jesse.chan@rice.edu>
2022-10-25 21:23:25 -0500

    add more cut cell tests

--- test/cut_mesh_tests.jl
+++ test/cut_mesh_tests.jl
@@ -9,7 +9,28 @@ using PathIntersections
     rd = RefElemData(Quad(), N=3)
     md = MeshData(rd, (circle, ), cells_per_dimension_x, cells_per_dimension_y)
 
+    # check the volume of the domain
     A = 4 - pi * .33^2
     @test sum(md.wJq) ≈ A
-    
+
+    # check the length of the boundary of the domain
+    face_weights = reshape(rd.wf, :, num_faces(rd.element_type))[:, 1]
+    wJf = vec(Diagonal(face_weights) * reshape(md.Jf, length(face_weights), :))
+    @test sum(wJf[md.mapB]) ≈ (8 + 2 * pi * .33)
+
+    # check continuity of a function that's in the global polynomial space
+    @unpack physical_frame_elements = md.mesh_type
+    @unpack x, y = md
+    u = @. x^rd.N - x * y^(rd.N-1) - x^(rd.N-1) * y + y^rd.N
+    uf = similar(md.xf)
+    uf.cartesian .= rd.Vf * u.cartesian
+    for e in 1:size(md.x.cut, 2)
+        ids = md.mesh_type.cut_face_nodes[e]
+        VDM = vandermonde(physical_frame_elements[e], rd.N, md.x.cut[:, e], md.y.cut[:, e])
+        Vf = vandermonde(physical_frame_elements[e], rd.N, md.xf.cut[ids], md.yf.cut[ids]) / VDM
+        uf.cut[ids] .= Vf * u.cut[:, e]
+    end
+    @test all(uf .≈ vec(uf[md.mapP]))
+
+    # TODO: add tests on taking derivatives 
 end
