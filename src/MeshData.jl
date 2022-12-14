94d104bd53cab632fecedc8daee6a3183e05b0ff
Jesse Chan <jesse.chan@rice.edu>
2022-12-14 08:52:56 -0600

    removing unnecessary function

--- src/MeshData.jl
+++ src/MeshData.jl
@@ -64,10 +64,6 @@ function MeshData(mesh_type, VXYZ, EToV, FToF, xyz, xyzf, xyzq, wJq,
                     rstxyzJ, J, nxyz, nxyzJ, Jf, is_periodic)
 end
 
-# enable use of @set and setproperties(...) for MeshData
-ConstructionBase.constructorof(::Type{MeshData{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10}}) where {T1, T2, T3, T4, T5, T6, T7, T8, T9, T10} = 
-    MeshData{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10}
-
 function ConstructionBase.setproperties(md::MeshData, patch::NamedTuple)
     fields = (haskey(patch, symbol) ? getproperty(patch, symbol) : getproperty(md, symbol) for symbol in fieldnames(typeof(md)))
     return MeshData(fields...)
