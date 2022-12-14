0f1b74d54efba50bb8f8a1b72e86ec1abf14157f
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2022-12-14 09:16:13 -0600

    Remove some redundant tests and functions (#62)

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,6 @@ using Triangulate # load before StartUpDG b/c of @require
 using StartUpDG
 
 include("triangulate_tests.jl")
-include("geometric_term_tests.jl")
 include("reference_elem_tests.jl")
 include("sbp_tests.jl")
 include("MeshData_tests.jl")
