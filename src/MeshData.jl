6bfb63a0f9a769fd03c13559ba9ef7572af56b19
Jesse Chan <jesse.chan@rice.edu>
2023-01-02 11:29:37 -0600

    adding VertexMappedMesh to MeshData

--- src/MeshData.jl
+++ src/MeshData.jl
@@ -13,18 +13,18 @@ md = MeshElemData(VXY, EToV, rd)
 @unpack x, y = md
 ```
 """
-Base.@kwdef struct MeshData{Dim, MeshType, VolumeType, FaceType, VolumeQType,
-                            VertexType, EToVType, FToFType, 
-                            VolumeWeightType, VolumeGeofacsType, VolumeJType,
+Base.@kwdef struct MeshData{Dim, MeshType, 
+                            VolumeType, FaceType, VolumeQType, FToFType, 
+                            VolumeWeightType, VolumeGeofacsType, VolumeJType, 
                             ConnectivityTypeM, ConnectivityTypeP, BoundaryMapType}
 
-    # this field defaults to the element shape, but can be 
-    # used to specify cut-cell, hybrid, non-conforming meshes. 
+    # this field defaults to the element shape, but can be used to specify 
+    # different mesh types (e.g., vertex-mapped, cut-cell, hybrid, non-conforming meshes. 
     mesh_type::MeshType 
 
-    # TODO: move VXYZ, EToV into a `VertexMappedMesh` mesh_type?
-    VXYZ::NTuple{Dim, VertexType}   # vertex coordinates
-    EToV::EToVType                  # mesh vertex array     
+    # # TODO: move VXYZ, EToV into a `VertexMappedMesh` mesh_type?
+    # VXYZ::NTuple{Dim, VertexType}   # vertex coordinates
+    # EToV::EToVType                  # mesh vertex array     
 
     FToF::FToFType                  # face connectivity
 
@@ -53,15 +53,12 @@ Base.@kwdef struct MeshData{Dim, MeshType, VolumeType, FaceType, VolumeQType,
     is_periodic::NTuple{Dim, Bool}
 end
 
-# MeshData constructor where we do not specify `nxyz`
-function MeshData(mesh_type, VXYZ, EToV, FToF, xyz, xyzf, xyzq, wJq, 
-                  mapM, mapP, mapB, rstxyzJ, J, nxyzJ, Jf, is_periodic) 
+# MeshData constructor where we do not specify `nxyz` and instead compute it from `nrstJ` and `Jf`
+function MeshData(mesh_type, FToF, xyz, xyzf, xyzq, wJq, mapM, mapP, mapB, rstxyzJ, J, nxyzJ, Jf, is_periodic) 
 
     nxyz = map(nJ -> nJ ./ Jf, nxyzJ)        
                          
-    return MeshData(mesh_type, VXYZ, EToV, FToF, 
-                    xyz, xyzf, xyzq, wJq, mapM, mapP, mapB,
-                    rstxyzJ, J, nxyz, nxyzJ, Jf, is_periodic)
+    return MeshData(mesh_type, FToF, xyz, xyzf, xyzq, wJq, mapM, mapP, mapB, rstxyzJ, J, nxyz, nxyzJ, Jf, is_periodic)
 end
 
 function ConstructionBase.setproperties(md::MeshData, patch::NamedTuple)
@@ -70,7 +67,7 @@ function ConstructionBase.setproperties(md::MeshData, patch::NamedTuple)
 end
 
 ConstructionBase.getproperties(md::MeshData) = 
-    (; mesh_type=md.mesh_type, VXYZ=md.VXYZ, EToV=md.EToV, FToF=md.FToF, xyz=md.xyz, xyzf=md.xyzf, xyzq=md.xyzq, wJq=md.wJq,
+    (; mesh_type=md.mesh_type, FToF=md.FToF, xyz=md.xyz, xyzf=md.xyzf, xyzq=md.xyzq, wJq=md.wJq,
        mapM=md.mapM, mapP=md.mapP, mapB=md.mapB, rstxyzJ=md.rstxyzJ, J=md.J, nxyz = md.nxyz, nxyzJ=md.nxyzJ, Jf=md.Jf,
        is_periodic=md.is_periodic)
 
@@ -81,7 +78,7 @@ end
 
 function Base.show(io::IO, ::MIME"text/plain", md::MeshData{DIM, MeshType}) where {DIM, MeshType}
     @nospecialize md
-    print(io,"$MeshType MeshData of dimension $DIM with $(md.num_elements) elements")
+    print(io,"MeshData of dimension $DIM with $(md.num_elements) elements")
 end
 
 function Base.propertynames(x::MeshData{1}, private::Bool = false)
@@ -99,85 +96,105 @@ function Base.propertynames(x::MeshData{3}, private::Bool = false)
             :nx, :ny, :nz, :rxJ, :sxJ, :txJ, :ryJ, :syJ, :tyJ, :rzJ, :szJ, :tzJ)
 end
 
-# convenience routines for unpacking individual tuple entries
-function Base.getproperty(x::MeshData, s::Symbol)
+struct VertexMappedMesh{TE <: AbstractElemShape, TV, TEV}
+    element_type::TE
+    VXYZ::TV
+    EToV::TEV
+end
 
-    if s==:VX
-        return getfield(x, :VXYZ)[1]
-    elseif s==:VY
-        return getfield(x, :VXYZ)[2]
-    elseif s==:VZ
-        return getfield(x, :VXYZ)[3]
+# generic `getproperty` specializations. uses a macro to avoid type instability. for more detail, see 
+#   https://discourse.julialang.org/t/type-stable-specializations-of-getproperty-without-code-duplication/92404/6)
+macro meshdata_getproperty(x, s) 
+    return :(
+        if $(esc(s))===:x
+            return getfield($(esc(x)), :xyz)[1]
+        elseif $(esc(s))===:y
+            return getfield($(esc(x)), :xyz)[2]
+        elseif $(esc(s))===:z
+            return getfield($(esc(x)), :xyz)[3]
 
-    elseif s==:x
-        return getfield(x, :xyz)[1]
-    elseif s==:y
-        return getfield(x, :xyz)[2]
-    elseif s==:z
-        return getfield(x, :xyz)[3]
+        elseif $(esc(s))===:xq
+            return getfield($(esc(x)), :xyzq)[1]
+        elseif $(esc(s))===:yq
+            return getfield($(esc(x)), :xyzq)[2]
+        elseif $(esc(s))===:zq
+            return getfield($(esc(x)), :xyzq)[3]
 
-    elseif s==:xq
-        return getfield(x, :xyzq)[1]
-    elseif s==:yq
-        return getfield(x, :xyzq)[2]
-    elseif s==:zq
-        return getfield(x, :xyzq)[3]
+        elseif $(esc(s))===:xf
+            return getfield($(esc(x)), :xyzf)[1]
+        elseif $(esc(s))===:yf
+            return getfield($(esc(x)), :xyzf)[2]
+        elseif $(esc(s))===:zf
+            return getfield($(esc(x)), :xyzf)[3]
 
-    elseif s==:xf
-        return getfield(x, :xyzf)[1]
-    elseif s==:yf
-        return getfield(x, :xyzf)[2]
-    elseif s==:zf
-        return getfield(x, :xyzf)[3]
+        elseif $(esc(s))===:nxJ
+            return getfield($(esc(x)), :nxyzJ)[1]
+        elseif $(esc(s))===:nyJ
+            return getfield($(esc(x)), :nxyzJ)[2]
+        elseif $(esc(s))===:nzJ
+            return getfield($(esc(x)), :nxyzJ)[3]
 
-    elseif s==:nxJ
-        return getfield(x, :nxyzJ)[1]
-    elseif s==:nyJ
-        return getfield(x, :nxyzJ)[2]
-    elseif s==:nzJ
-        return getfield(x, :nxyzJ)[3]
+        elseif $(esc(s))===:nx
+            return getfield($(esc(x)), :nxyz)[1]
+        elseif $(esc(s))===:ny
+            return getfield($(esc(x)), :nxyz)[2]
+        elseif $(esc(s))===:nz
+            return getfield($(esc(x)), :nxyz)[3]        
 
-    elseif s==:nx
-        return getfield(x, :nxyz)[1]
-    elseif s==:ny
-        return getfield(x, :nxyz)[2]
-    elseif s==:nz
-        return getfield(x, :nxyz)[3]        
+        elseif $(esc(s))===:rxJ
+            return getfield($(esc(x)), :rstxyzJ)[1,1]
+        elseif $(esc(s))===:sxJ
+            return getfield($(esc(x)), :rstxyzJ)[1,2]
+        elseif $(esc(s))===:txJ
+            return getfield($(esc(x)), :rstxyzJ)[1,3]
+        elseif $(esc(s))===:ryJ
+            return getfield($(esc(x)), :rstxyzJ)[2,1]
+        elseif $(esc(s))===:syJ
+            return getfield($(esc(x)), :rstxyzJ)[2,2]
+        elseif $(esc(s))===:tyJ
+            return getfield($(esc(x)), :rstxyzJ)[2,3]
+        elseif $(esc(s))===:rzJ
+            return getfield($(esc(x)), :rstxyzJ)[3,1]
+        elseif $(esc(s))===:szJ
+            return getfield($(esc(x)), :rstxyzJ)[3,2]
+        elseif $(esc(s))===:tzJ
+            return getfield($(esc(x)), :rstxyzJ)[3,3]
+            
+        # old behavior where K = num_elements        
+        elseif $(esc(s))===:K || $(esc(s))===:num_elements 
+            return num_elements($(esc(x)))
 
-    elseif s==:rxJ
-        return getfield(x, :rstxyzJ)[1,1]
-    elseif s==:sxJ
-        return getfield(x, :rstxyzJ)[1,2]
-    elseif s==:txJ
-        return getfield(x, :rstxyzJ)[1,3]
-    elseif s==:ryJ
-        return getfield(x, :rstxyzJ)[2,1]
-    elseif s==:syJ
-        return getfield(x, :rstxyzJ)[2,2]
-    elseif s==:tyJ
-        return getfield(x, :rstxyzJ)[2,3]
-    elseif s==:rzJ
-        return getfield(x, :rstxyzJ)[3,1]
-    elseif s==:szJ
-        return getfield(x, :rstxyzJ)[3,2]
-    elseif s==:tzJ
-        return getfield(x, :rstxyzJ)[3,3]
-        
-    # old behavior where K = num_elements        
-    elseif s==:K || s==:num_elements 
-        return num_elements(x)
+        # old notation in the NDG book where sJ (surface Jacobian) is 
+        # used instead of Jf (Jacobian for the face)
+        elseif $(esc(s))===:sJ 
+            return getfield($(esc(x)), :Jf)
 
-    # old notation in the NDG book where sJ (surface Jacobian) is 
-    # used instead of Jf (Jacobian for the face)
-    elseif s==:sJ 
-        return getfield(x, :Jf)
+        else
+            return getfield($(esc(x)), $(esc(s)))
+        end
+    ) # end quote
+end
 
+# generic fallback
+Base.getproperty(x::MeshData, s::Symbol) = @meshdata_getproperty(x, s)
+
+# convenience routines for VertexMappedMeshes
+function Base.getproperty(x::MeshData{Dim, <:VertexMappedMesh}, s::Symbol) where {Dim}
+
+    if s===:VX
+        return getfield(getfield(x, :mesh_type), :VXYZ)[1]
+    elseif s===:VY
+        return getfield(getfield(x, :mesh_type), :VXYZ)[2]
+    elseif s===:VZ
+        return getfield(getfield(x, :mesh_type), :VXYZ)[3]
+    elseif s===:EToV
+        return getfield(getfield(x, :mesh_type), s)
     else
-        return getfield(x, s)
+        @meshdata_getproperty(x, s)
     end
 end
 
-num_elements(md) = size(getfield(md, :EToV), 1)
+num_elements(md::MeshData{Dim, <:VertexMappedMesh}) where {Dim} = size(md.mesh_type.EToV, 1)
 
 """
     MeshData(VXYZ, EToV, rd::RefElemData)
@@ -236,7 +253,7 @@ function MeshData(VX::AbstractVector{Tv}, EToV, rd::RefElemData{1}) where {Tv}
 
     is_periodic = (false,)
 
-    return MeshData(rd.element_type, tuple(VX), EToV, FToF,
+    return MeshData(VertexMappedMesh(rd.element_type, tuple(VX), EToV), FToF,
                     tuple(x), tuple(xf), tuple(xq), wJq,
                     collect(mapM), mapP, mapB,
                     SMatrix{1,1}(tuple(rxJ)), J,
@@ -276,7 +293,7 @@ function MeshData(VX, VY, EToV, rd::RefElemData{2})
     nxJ, nyJ, Jf = compute_normals(rstxyzJ, rd.Vf, rd.nrstJ...)
 
     is_periodic = (false, false)
-    return MeshData(rd.element_type, tuple(VX, VY), EToV, FToF,
+    return MeshData(VertexMappedMesh(rd.element_type, tuple(VX, VY), EToV), FToF,
                     tuple(x, y), tuple(xf, yf), tuple(xq, yq), wJq,
                     mapM, mapP, mapB,
                     SMatrix{2, 2}(tuple(rxJ, ryJ, sxJ, syJ)), J,
@@ -314,13 +331,14 @@ function MeshData(VX, VY, VZ, EToV, rd::RefElemData{3})
     nxJ, nyJ, nzJ, Jf = compute_normals(rstxyzJ, rd.Vf, rd.nrstJ...)
 
     is_periodic = (false, false, false)
-    return MeshData(rd.element_type, tuple(VX, VY, VZ), EToV, FToF,
+    return MeshData(VertexMappedMesh(rd.element_type, tuple(VX, VY, VZ), EToV), FToF,
                     tuple(x, y, z), tuple(xf, yf, zf), tuple(xq, yq, zq), wJq,
                     mapM, mapP, mapB,
                     rstxyzJ, J, tuple(nxJ, nyJ, nzJ), Jf,
                     is_periodic)
 end
 
+# TODO: remove with breaking change v0.15
 @deprecate MeshData(md::MeshData, rd::RefElemData, xyz...) MeshData(rd, md, xyz...)
 
 function recompute_geometry(rd::RefElemData{Dim}, xyz) where {Dim}
