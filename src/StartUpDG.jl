0e6ac94f3903a47f3d23699a91dcf2a4becef865
Hendrik Ranocha <mail@ranocha.de>
2023-01-06 16:31:47 +0100

    make some using statements more explicit

--- src/StartUpDG.jl
+++ src/StartUpDG.jl
@@ -1,11 +1,11 @@
 module StartUpDG
 
-using Reexport 
+using Reexport
 
-using Colors 
+using Colors: HSV, distinguishable_colors
 @reexport using ComponentArrays: ComponentArray
 using ConstructionBase: ConstructionBase
-using HDF5 # used to read in SBP triangular node data
+using HDF5: h5open # used to read in SBP triangular node data
 using Kronecker: kronecker # for Hex element matrix manipulations
 using LinearAlgebra: cond, diagm, eigvals, Diagonal, I, mul!, norm, qr, ColumnNorm
 using NodesAndModes: meshgrid, find_face_nodes, face_vertices
@@ -13,8 +13,8 @@ using NodesAndModes: meshgrid, find_face_nodes, face_vertices
 using OrderedCollections: LittleDict # fast ordered dict for a small number of entries
 using PathIntersections
 @reexport using PathIntersections: PresetGeometries
-using Printf
-using RecipesBase
+using Printf: @sprintf
+using RecipesBase: RecipesBase, @series
 using StaticArrays: SVector, SMatrix
 using Setfield: setproperties, @set # for "modifying" structs (setproperties)
 using SparseArrays: sparse, droptol!, blockdiag
@@ -61,23 +61,23 @@ export num_mortars_per_face, NonConformingQuadMeshExample
 # uniform meshes + face vertex orderings
 include("mesh/simple_meshes.jl")
 export readGmsh2D, uniform_mesh
-export readGmsh2D_v4, MeshImportOptions 
+export readGmsh2D_v4, MeshImportOptions
 
 # Plots.jl recipes for meshes
 include("mesh/mesh_visualization.jl")
 export VertexMeshPlotter, MeshPlotter
 
 # Triangulate interfaces and pre-built meshes
-include("mesh/triangulate_utils.jl")      
+include("mesh/triangulate_utils.jl")
 export refine, triangulateIO_to_VXYEToV, get_node_boundary_tags
 export BoundaryTagPlotter
 include("mesh/triangulate_example_meshes.jl")
 export triangulate_domain
-export Scramjet, SquareDomain, RectangularDomain, RectangularDomainWithHole 
+export Scramjet, SquareDomain, RectangularDomain, RectangularDomainWithHole
 export CircularDomain, PartialCircularDomain
 
 # simple explicit time-stepping included for conveniencea
 include("explicit_timestep_utils.jl")
-export ck45 # LSERK 45 
+export ck45 # LSERK 45
 
 end
