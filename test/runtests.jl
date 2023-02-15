cc4b1f75bf76d4c70f8c192f7edfa20baa5661d4
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2023-02-15 09:33:33 -0600

    Clean up VTK utilities and tests (#87)
    * clean up tests and vtk writer
    
    * spelling
    
    * fix type instability

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,9 @@
 using Test
 using Suppressor
 using LinearAlgebra
-using RecipesBase
-using Triangulate 
+using RecipesBase: RecipesBase
 using StartUpDG
-using WriteVTK
 
-
-include("../src/mesh/vtk_helper.jl")
 include("write_vtk_tests.jl")
 include("named_array_partition_tests.jl")
 include("triangulate_tests.jl")
