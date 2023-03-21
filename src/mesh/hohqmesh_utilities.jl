ad98e842cc63f9f3afb8df6e7586ad2ce1230d61
Jesse Chan <jesse.chan@rice.edu>
2023-03-21 15:42:02 -0700

    remove redundant code

--- src/mesh/hohqmesh_utilities.jl
+++ src/mesh/hohqmesh_utilities.jl
@@ -76,16 +76,22 @@ struct CurvedHOHQMeshElement
     curved_edge_coordinates::Matrix{Float64}
 end
 
-function _read_HOHQMesh(lines, ::ISM_V2)    
-    num_nodes, num_edges, nelements, polydeg = parse.(Int, split(popat!(lines, 1)))
+function _read_HOHQMesh(lines, mesh_format)
+    if mesh_format isa ISM_V2
+        num_nodes, num_edges, nelements, polydeg = parse.(Int, split(popat!(lines, 1)))
+    else
+        num_nodes, nelements, polydeg = parse.(Int, split(popat!(lines, 1)))
+    end
     VX, VY, VZ = ntuple(_ -> zeros(num_nodes), 3)
     for i in 1:num_nodes
         VX[i], VY[i], VZ[i] = parse.(Float64, split(lines[i]))
     end
     deleteat!(lines, 1:num_nodes)
 
-    # ignore edges for now 
-    deleteat!(lines, 1:num_edges)
+    if mesh_format isa ISM_V2
+        # ignore edges for now 
+        deleteat!(lines, 1:num_edges)
+    end
 
     curved_elements = CurvedHOHQMeshElement[]
     nvertices = length(split(lines[1]))    
@@ -94,14 +100,16 @@ function _read_HOHQMesh(lines, ::ISM_V2)
     boundary_tags = Matrix{String}(undef, nelements, num_faces)
     for e in 1:nelements
         EToV[e, :] .= parse.(Int, split(lines[1]))
-        curved_edges = parse.(Int, split(lines[2]))
+        curved_edges = parse.(Bool, split(lines[2]))
 
         deleteat!(lines, 1:2) # move onto next lines
 
         if all(curved_edges .== 0)
             # do nothing
         else
-            curved_edge_coordinates = mapreduce(vcat, lines[1:polydeg+1]) do s
+            num_curved_faces = count(curved_edges)
+            curved_nodes = 1:(polydeg+1) * num_curved_faces
+            curved_edge_coordinates = mapreduce(vcat, lines[curved_nodes]) do s
                 (parse.(Float64, split(s)))'
             end
             push!(curved_elements, 
@@ -121,47 +129,3 @@ function _read_HOHQMesh(lines, ::ISM_V2)
         return (VX, VY, VZ), EToV, polydeg, curved_elements, boundary_tags
     end
 end
-
-function _read_HOHQMesh(lines, ::ISM)    
-    num_nodes, nelements, polydeg = parse.(Int, split(popat!(lines, 1)))
-    VX, VY, VZ = ntuple(_ -> zeros(num_nodes), 3)
-    for i in 1:num_nodes
-        VX[i], VY[i], VZ[i] = parse.(Float64, split(lines[i]))
-    end
-    deleteat!(lines, 1:num_nodes)
-
-    curved_elements = CurvedHOHQMeshElement[]
-    nvertices = length(split(lines[1]))    
-    num_faces = nvertices == 4 ? 4 : 6 # in 2D, 4 faces. In 3D, 6 faces
-    EToV = zeros(Int, nelements, nvertices)
-    boundary_tags = Matrix{String}(undef, nelements, num_faces)
-    for e in 1:nelements
-        EToV[e, :] .= parse.(Int, split(lines[1]))
-        curved_edges = parse.(Int, split(lines[2]))
-
-        deleteat!(lines, 1:2) # move onto next lines
-
-        if all(curved_edges .== 0)
-            # do nothing
-        else
-            curved_edge_coordinates = mapreduce(vcat, lines[1:polydeg+1]) do s
-                (parse.(Float64, split(s)))'
-            end
-            push!(curved_elements, 
-                  CurvedHOHQMeshElement(e, curved_edges, curved_edge_coordinates))
-            deleteat!(lines, 1:polydeg+1) # move on to next lines
-        end
-
-        tags = split(popat!(lines, 1))
-        for i in eachindex(tags)
-            boundary_tags[e, i] = tags[i]
-        end        
-    end
-    if nvertices == 4
-        return (VX, VY), EToV, polydeg, curved_elements, boundary_tags
-    else
-        return (VX, VY, VZ), EToV, polydeg, curved_elements, boundary_tags
-    end
-end
-
-        
