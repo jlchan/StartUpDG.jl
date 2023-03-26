d71c85cf253d62fd9b0e9304d0c37b160ea299e2
Jesse Chan <jesse.chan@rice.edu>
2023-03-26 18:43:30 -0500

    add tests

--- test/hohqmesh_tests.jl
+++ test/hohqmesh_tests.jl
@@ -1,15 +1,18 @@
 @testset "HOHQMesh" begin 
-    rd = RefElemData(Quad(), 4)
 
     filename = "testset_HOHQMesh_meshes/easy_example.mesh"
+    # filename = "test/testset_HOHQMesh_meshes/easy_example.mesh"
     hmd = read_HOHQMesh(filename)
+    rd = RefElemData(Quad(), 4)
     md = MeshData(hmd, rd)
     area = 16 - 0.25^2 * pi
     @test all(md.J .> 0)
     @test abs(sum(md.wJq) - area) < 1e-4
 
     filename = "testset_HOHQMesh_meshes/GingerbreadMan.mesh"
+    # filename = "test/testset_HOHQMesh_meshes/GingerbreadMan.mesh"
     hmd = read_HOHQMesh(filename)
+    rd = RefElemData(Quad(), 4)
     md = MeshData(hmd, rd)    
     @test sum(md.wJq) ≈ 1060.558197978162
 
@@ -20,7 +23,11 @@
 
     # Tri and Tet meshes
     filename = "testset_HOHQMesh_meshes/MSMappedTri4P4.mesh"
+    # filename = "test/testset_HOHQMesh_meshes/MSMappedTri4P4.mesh"
     hmd = read_HOHQMesh(filename, Tri())
+    rd = RefElemData(Tri(), 4)
+    md = MeshData(hmd, rd)
+    @test all(md.J .> 0)
 
     filename = "testset_HOHQMesh_meshes/MSMappedTet4P4.mesh"
     hmd = read_HOHQMesh(filename, Tet())
