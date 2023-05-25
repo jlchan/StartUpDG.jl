7f6c3dd25d18506d10896210f7bbdc0eeb2b4c4b
David Knapp <david.knapp@dlr.de>
2023-05-25 14:18:11 +0200

    Add Comments

--- src/mesh/mesh_visualization.jl
+++ src/mesh/mesh_visualization.jl
@@ -113,9 +113,19 @@ function MeshData_to_vtk(md::MeshData, rd::RefElemData{DIM}, data, dataname, fil
     return vtk_save(vtkfile)
 end
 
-# Special function to write vtk-data for TensorProductWedges. For each wedge we construct a set of linear `VTK_WEDGE` given
-# the nodes of the triangulare base and the linear nodes. 
-function MeshData_Tensor_to_vtk(md::MeshData, rd::RefElemData{DIM}, data, dataname, filename, write_data = false, equi_dist_nodes = false) where {DIM}
+"""
+MeshData_Tensor_to_vtk(md, rd, dim, data, dataname, datatype, filename, write_data = false, equi_dist_nodes = true)
+
+Translate the given mesh into a vtk-file.
+`md` holds a `MeshData` object
+`rd` holds a reference element data/`RefElemData` of a TensorProductWedge
+`data` holds an array of arrays (of size `num_nodes` by `num_elements`) with plotting data
+`dataname` is an array of strings with name of the associated data
+`write_data`, flag if data should be written or not (e.g., if data is not written, only the mesh will be saved as output)
+`equi_dist_nodes` flag if points should be interpolated to equidstant nodes
+"""
+function MeshData_Tensor_to_vtk(md::MeshData, rd::RefElemData{DIM}, data, dataname, filename, 
+                    write_data = false, equi_dist_nodes = false) where {DIM}
     # Number of nodes in the triangular base
     num_tri_nodes = length(rd.approximation_type.tri.r)
     # Number of all wedges in the mesh
