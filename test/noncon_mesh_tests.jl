ebaf7af31475ddfdd8efd128ed2fada7a5f18b3a
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2023-06-13 15:36:55 -0500

    Switch to concatenated nonconforming interface treatment (#140)
    * use concatenated nonconforming treatment
    
    * use `num_mortars_per_face`
    
    * comments and test update
    
    * remove EToV from mesh_type
    
    * cleanup test

--- test/noncon_mesh_tests.jl
+++ test/noncon_mesh_tests.jl
@@ -3,27 +3,22 @@
     rd = RefElemData(Quad(), N=3)
     md = MeshData(NonConformingQuadMeshExample(), rd)
 
-    (; x, y  ) = md
+    (; x, y) = md
     u = @. sin(pi * x) * sin(pi * y)
 
     # interpolate to faces
     num_total_faces = num_faces(rd.element_type) * md.num_elements
-    u_face = reshape(rd.Vf * u, :, num_total_faces)
+    uf = reshape(rd.Vf * u, :, num_total_faces)
 
     # interpolate faces to mortars (`uf` denotes mortar faces for `NonConformingMesh` types)
-    (; conforming_faces, nonconforming_faces, mortar_interpolation_matrix  ) = md.mesh_type
-
-    u_mortar = similar(md.xf)
-    view(u_mortar, :, 1:length(conforming_faces)) .= view(u_face, :, conforming_faces)
-    # interpolate to non-conforming faces
-    for (i, f) in enumerate(nonconforming_faces)
-        mortar_face_ids = (1:num_mortars_per_face(rd)) .+ (i-1) * num_mortars_per_face(rd) .+ length(conforming_faces)
-        u_mortar[:, mortar_face_ids] .= reshape(mortar_interpolation_matrix * u_face[:, f], :, num_mortars_per_face(rd))
-    end
-
-    # get exterior values
-    uP = u_mortar[md.mapP]
+    (; nonconforming_faces, mortar_interpolation_matrix) = md.mesh_type
+    u_mortar = reshape(mortar_interpolation_matrix * uf[:, nonconforming_faces], :, 
+                       num_mortars_per_face(rd) * length(nonconforming_faces))
+    
+    # construct interior (uM = u⁻ "minus") values and exterior (uP = u⁺ "plus") values
+    uM = hcat(uf, u_mortar)
+    uP = uM[md.mapP]
 
     # check that the jumps are zero for continuous u with zero boundary values
-    @test maximum(abs.(uP - u_mortar)) < 100 * eps()
+    @test maximum(abs.(uP - uM)) < 100 * eps()
 end
