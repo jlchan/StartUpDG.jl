3f717ba7dbf97db72da620d1754d54d2215136cd
Jesse Chan <jesse.lee.chan@gmail.com>
2024-05-16 10:50:13 -0500

    add docstring

--- src/cut_cell_meshes.jl
+++ src/cut_cell_meshes.jl
@@ -1009,6 +1009,17 @@ function MeshData(rd::RefElemData, objects,
 
 end
 
+"""
+    hybridized_SBP_operators(md::MeshData{2, <:CutCellMesh})
+
+This constructs hybridized SBP operators using the approach taken in Chan (2019), 
+"Skew-Symmetric Entropy Stable Modal Discontinuous Galerkin Formulations". 
+[https://doi.org/10.1007/s10915-019-01026-w](https://doi.org/10.1007/s10915-019-01026-w)
+
+This function returns `hybridized_operators::Vector{Tuple{<:Matrix, <:Matrix}}` and 
+`project_and_interp_operators, projection_operators, interpolation_operators`, which are 
+all `Vector{<:Matrix}`, where each entry corresponds to a cut element.
+"""
 function hybridized_SBP_operators(md::MeshData{2, <:CutCellMesh})
     mt = md.mesh_type
     (; volume_interpolation_matrices, 
