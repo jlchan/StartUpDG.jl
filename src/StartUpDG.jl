8f86d1ceb06bd5b8a4b612e7889cbc6c2aba4724
Jesse Chan <jesse.lee.chan@gmail.com>
2024-05-21 17:15:18 -0500

    remove requires statements in StartUpDG.jl

--- src/StartUpDG.jl
+++ src/StartUpDG.jl
@@ -103,20 +103,4 @@ export CircularDomain, PartialCircularDomain
 include("explicit_timestep_utils.jl")
 export ck45 # LSERK 45
 
-
-using Requires: @require
-
-function __init__()
-    @require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
-        include("TriangulatePlots.jl")
-    end
-
-    # Until Julia v1.9 is the minimum required version for StartUpDG.jl, we still support Requires.jl
-    @static if !isdefined(Base, :get_extension)
-        @require SummationByPartsOperators = "9f78cca6-572e-554e-b819-917d2f1cf240" begin
-            include("../ext/StartUpDGSummationByPartsOperatorsExt.jl")
-        end
-    end
-end
-
 end # module
