684ede1e4d9c97d6942dab5179301f1c504f69ff
Jesse Chan <jesse.lee.chan@gmail.com>
2025-04-07 13:51:32 -0500

    clean up vtk tests

--- test/write_vtk_tests.jl
+++ test/write_vtk_tests.jl
@@ -1,9 +1,5 @@
 @testset "VTK tests" begin
 
-    function quad(x, y)
-        return (x + y)^2
-    end
-
     # test expected output of `vtk_order`
     deg_one_order(::Tri) = permutedims(hcat(nodes(Tri(), 1)...))
     deg_zero_order(::Tri) = [-1.0; -1.0]
@@ -21,16 +17,36 @@
 
 
     @testset "VTKWriter test for $elem" for elem in [Tri(), Quad(), Hex(), Wedge(), Tet()]
-        N = 2 # test only N=2 for CI time
         @testset "Write Mesh" begin
-            rd = RefElemData(elem, N)
+            function quad(x, y)
+                return (x + y)^2
+            end
+            rd = RefElemData(elem, 2) # test only N=2 for CI time
             md = MeshData(uniform_mesh(elem, 2)..., rd)
             interpolate = vandermonde(rd.element_type, rd.N, equi_nodes(rd.element_type, rd.N)...) / rd.VDM
             pdata = [quad.(interpolate * md.x, interpolate * md.y)]
-            filename = replace(string(elem), "()" => "") * "_" * string(N)
+            filename = replace(string(elem), "()" => "") * "_" * string(rd.N)
             check = filename * ".vtu"
             # Todo: Can we implement a better check?
             vtu_name = MeshData_to_vtk(md, rd, pdata, ["(x+y)^2"], filename, true)
+            @test vtu_name[1] == check
+            rm(check) # remove created file after test is done
+
+            # ======= check `export_to_vtk` interfaces
+            
+            # copy of MeshData_to_vtk but with rd, md reversed for consistency
+            vtu_name = export_to_vtk(rd, md, pdata, ["(x+y)^2"], filename)
+            @test vtu_name[1] == check
+            rm(check) # remove created file after test is done
+
+            # without filename specified
+            vtu_name = export_to_vtk(rd, md, pdata, filename)            
+            @test vtu_name[1] == check
+            rm(check) # remove created file after test is done
+
+            # with data/dataname specified via Dict
+            data = quad.(interpolate * md.x, interpolate * md.y)
+            vtu_name = export_to_vtk(rd, md, Dict("(x+y)^2" => data), filename)            
             @test vtu_name[1] == check
             rm(check) # remove created file after test is done
         end
@@ -48,14 +64,14 @@
     end
 
     @testset "TensorProduct VTKWriter" begin
-        @testset "Degree ($tri_grad, $line_grad)" for tri_grad in 2, line_grad in 3
-            line = RefElemData(Line(), line_grad)
-            tri  = RefElemData(Tri(), tri_grad)
+        @testset "Degree ($tri_N, $line_N)" for tri_N in 2, line_N in 3
+            line = RefElemData(Line(), line_N)
+            tri  = RefElemData(Tri(), tri_N)
             tensor = TensorProductWedge(tri, line)
             rd = RefElemData(Wedge(), tensor)
             md = MeshData(uniform_mesh(Wedge(), 2)..., rd)
             pdata = [quad.(md.x, md.y)]
-            filename = "TensorProductWedge" * "_" * string(tri_grad) * "_" * string(line_grad)
+            filename = "TensorProductWedge" * "_" * string(tri_N) * "_" * string(line_N)
             check = filename * ".vtu"
             # Todo: Can we implement a better check?
             vtu_name = MeshData_to_vtk(md, rd, pdata, ["(x+y)^2"], filename, true, true)
