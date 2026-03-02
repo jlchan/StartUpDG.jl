31ea198944e0a767e26b1aa1b24f3bbe0660bfe6
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-03-02 17:50:27 +0100

    use at__DIR__

--- test/gmsh_parse_tests.jl
+++ test/gmsh_parse_tests.jl
@@ -1,36 +1,36 @@
 # gmsh test files are located in StartUpDG/test/testset_Gmsh_meshes/
-# malpasset.msh was previously a 2.2 file. Exported version 
+# malpasset.msh was previously a 2.2 file. Exported version
 # of 4.1 has been added for testing
 
-@testset "Gmsh" begin 
+@testset "Gmsh" begin
     @testset "Gmsh reading" begin
-        VXY, EToV = read_Gmsh_2D("testset_Gmsh_meshes/squareCylinder2D.msh")
+        VXY, EToV = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "squareCylinder2D.msh"))
         @test size(EToV) == (3031, 3)
 
-        # malpasset data taken from 
+        # malpasset data taken from
         # https://github.com/li12242/NDG-FEM/blob/master/Application/SWE/SWE2d/Benchmark/%40Malpasset2d/mesh/triMesh/malpasset.msh
-        VXY, EToV = read_Gmsh_2D("testset_Gmsh_meshes/malpasset.msh")
+        VXY, EToV = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "malpasset.msh"))
         rd = RefElemData(Tri(), 1)
         md = MeshData(VXY, EToV, rd)
         @test all(md.J .> 0)
     end
-    
+
     @testset "test gmsh element id remapping" begin
         testvec = [16, 15, 16, 17, 18]
         @test StartUpDG.remap_element_grouping(testvec) == [1, 2, 1, 3, 4]
     end
 
     @testset "$approxType MeshData initialization with gmsh import" for approxType = [Polynomial(), SBP()]
-        @testset "2D tri gmsh import verison $version" for version = [2.2, 4.1] 
+        @testset "2D tri gmsh import verison $version" for version = [2.2, 4.1]
             file = "pert_mesh"
             tol = 5e7*eps() # higher tolerance due to floating point issues?
             N = 3
 
             rd = RefElemData(Tri(), approxType, N)
             if version == 2.2
-                VXY, EToV = read_Gmsh_2D("testset_Gmsh_meshes/$file"*".msh");
+                VXY, EToV = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file"*".msh"));
             elseif version == 4.1
-                VXY, EToV = read_Gmsh_2D("testset_Gmsh_meshes/$file"*"_v4.msh");
+                VXY, EToV = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file"*"_v4.msh"));
             end
             md = MeshData(VXY, EToV, rd)
 
@@ -61,22 +61,22 @@
                 @test Vq*x ≈ xq
                 @test Vq*y ≈ yq
                 @test diagm(wq) * (Vq * J) ≈ wJq
-                #@test abs(sum(xq .* wJq)) < tol skip=true 
+                #@test abs(sum(xq .* wJq)) < tol skip=true
                 #@test abs(sum(yq .* wJq)) < tol skip=true
             end
 
             @testset "check surface integration begin" begin
                 @test Vf * x ≈ xf
                 @test Vf * y ≈ yf
-                @test abs(sum(wf .* nxJ)) < tol 
-                @test abs(sum(wf .* nyJ)) < tol 
+                @test abs(sum(wf .* nxJ)) < tol
+                @test abs(sum(wf .* nyJ)) < tol
                 #@test sum(@. wf * nxJ * (1 + xf) / 2) ≈ 2.0 skip=true # check sign of normals
             end
 
             @testset "check connectivity and boundary maps" begin
                 u = @. (1-x) * (1+x) * (1-y) * (1+y)
                 uf = Vf * u
-                @test uf ≈ uf[mapP]        
+                @test uf ≈ uf[mapP]
                 #@test norm(uf[mapB]) < tol skip=true
             end
 
@@ -97,14 +97,14 @@
             end
         end
     end
-    
+
     @testset "gmsh version 4.1 file with one data grouping" begin
-        file = "testset_Gmsh_meshes/one_group_v4.msh"
+        file = joinpath(@__DIR__, "testset_Gmsh_meshes", "one_group_v4.msh")
         if isfile(file)
             VXY_1, EToV_1 = read_Gmsh_2D(file)
-            VXY_2, EToV_2, group_2 = read_Gmsh_2D(file, true) 
-            @test VXY_1 == VXY_2 
-            @test EToV_1 == EToV_2 
+            VXY_2, EToV_2, group_2 = read_Gmsh_2D(file, true)
+            @test VXY_1 == VXY_2
+            @test EToV_1 == EToV_2
             f = open(file)
             lines = readlines(f)
             num_elements = StartUpDG.get_num_elements(lines)
@@ -116,14 +116,14 @@
     end;
 
     @testset "gmsh version 4.1 file with no grouping data" begin
-        # test promps for grouping data from the file. 
+        # test promps for grouping data from the file.
         # suppose such grouping data however is not in the file
-        file = "testset_Gmsh_meshes/no_group_v4.msh"
+        file = joinpath(@__DIR__, "testset_Gmsh_meshes", "no_group_v4.msh")
         if isfile(file)
             VXY_1, EToV_1 = read_Gmsh_2D(file)
-            VXY_2, EToV_2, group_2 = read_Gmsh_2D(file, true) 
-            @test VXY_1 == VXY_2 
-            @test EToV_1 == EToV_2 
+            VXY_2, EToV_2, group_2 = read_Gmsh_2D(file, true)
+            @test VXY_1 == VXY_2
+            @test EToV_1 == EToV_2
             f = open(file)
             lines = readlines(f)
             num_elements = StartUpDG.get_num_elements(lines)
@@ -136,11 +136,11 @@
 
     @testset "Compare output between v2.2 and v4.1" begin
         @testset "file:$file" for file in ["mesh_no_pert","pert_mesh","malpasset"]
-            if isfile("testset_Gmsh_meshes/$file.msh") && isfile("testset_Gmsh_meshes/$file"*"_v4.msh")
-                VXY_v2, EToV_v2 = read_Gmsh_2D("testset_Gmsh_meshes/$file.msh");
-                VXY_v4, EToV_v4 = read_Gmsh_2D("testset_Gmsh_meshes/$file" * "_v4.msh");
+            if isfile(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file.msh")) && isfile(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file"*"_v4.msh"))
+                VXY_v2, EToV_v2 = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file.msh"));
+                VXY_v4, EToV_v4 = read_Gmsh_2D(joinpath(@__DIR__, "testset_Gmsh_meshes", "$file" * "_v4.msh"));
                 @test VXY_v2 == VXY_v4
-                @test EToV_v2 == EToV_v4 
+                @test EToV_v2 == EToV_v4
             end
         end
     end
