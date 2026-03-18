6934e22f8a3d6a77082428dc74caadd3305a018d
Jesse Chan <jesse.lee.chan@gmail.com>
2026-03-18 16:13:39 +0100

    error if not interpolating when visualizing simplicial SBP types

--- src/mesh/visualization.jl
+++ src/mesh/visualization.jl
@@ -132,7 +132,11 @@ function MeshData_to_vtk(md::MeshData, rd::RefElemData, data, dataname, filename
         coords = map(x -> vec(rd.Vp * x), md.xyz)
         data = [rd.Vp * data_i for data_i in data]
     else # don't interpolate
-        coords = vec.(md.xyz) 
+        if (rd.approximation_type isa SBP) && (rd.element_type isa Union{Tri, Tet})
+            error("Support for non-interpolated SBP approximations is not supported on simplices.")
+        else # if polynomial
+            coords = vec.(md.xyz) 
+        end
     end   
     
     vtkfile = vtk_grid(filename, coords..., cells)
