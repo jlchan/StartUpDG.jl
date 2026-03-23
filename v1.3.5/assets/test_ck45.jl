177bf243c9317b15469d4e45144b942cc4ccb539
Documenter.jl <documenter@juliadocs.github.io>
2026-03-23 18:56:49 +0000

    build based on 1b45158

---
+++ v1.3.5/assets/test_ck45.jl
@@ -0,0 +1,29 @@
+using Plots
+using StartUpDG
+
+# Brusselator
+B = 3
+f(y1,y2) = [1+y1^2*y2-(B+1)*y1, B*y1-y1^2*y2]
+f(Q) = f(Q[1],Q[2])
+p,q = 1.5, 3.0
+Q = [p;q]
+
+dt = .1
+FinalTime = 20
+
+res = zero.(Q) # init RK residual
+rk4a,rk4b,rk4c = ck45()
+Nsteps = ceil(FinalTime/dt)
+dt = FinalTime/Nsteps
+
+plot() # init plot
+for i = 1:Nsteps
+    global res # yes, I know...this is just for simplicty
+    for INTRK=1:5
+        rhsQ = f(Q)
+        @. res = rk4a[INTRK]*res + dt*rhsQ # i = RK stage
+        @. Q =  Q + rk4b[INTRK]*res
+    end
+    scatter!([i*dt;i*dt],Q)
+end
+display(plot!(leg=false))
